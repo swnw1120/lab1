187d082db3f422661ac717ca65927c19d1d8a60b
swnw1120 <William.soong@warwick.ac.uk>
2018-10-07 18:58:25 +0100

    better

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -5,7 +5,15 @@ public class Hello {
         System.out.println("Hello World!222222222222");
         Scanner s = new Scanner(System.in);
         System.out.println("Name:");
-        String name = s.nextLine();
-        System.out.println("Hi, " + name);
+        if (s.hasNextLine()){
+          String name = s.nextLine();
+          if (name != null){
+            System.out.println("Hi, " + name);
+          }
+        }
+        else{
+            System.out.println("Hi, Noname");
+        }
+
     }
 }
