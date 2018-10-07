c86f27c93a0ed21090a434378810fa85071dcfff
swnw1120 <William.soong@warwick.ac.uk>
2018-10-07 13:57:52 +0100

    Input name

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!222222222222");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Name:");
+        String name = s.nextLine();
+        System.out.println("Hi, " + name);
     }
 }
